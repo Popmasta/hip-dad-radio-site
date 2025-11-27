6ab544deddcf700da976945cb5920e75a1ecded1
Popmasta <45741461+Popmasta@users.noreply.github.com>
2025-11-27 14:35:00 -0600

    Update player.js

--- player.js
+++ player.js
@@ -1,11 +1,12 @@
-const stream = document.getElementById("hdr-audio");
+const audio = document.getElementById("hdr-audio");
 const toggle = document.getElementById("playerToggle");
+
 toggle.addEventListener("click", () => {
-  if (stream.paused) {
-    stream.play();
+  if (audio.paused) {
+    audio.play();
     toggle.classList.add("playing");
   } else {
-    stream.pause();
+    audio.pause();
     toggle.classList.remove("playing");
   }
 });
